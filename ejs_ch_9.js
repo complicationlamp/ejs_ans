af65d9d039112fd796a4512820126e435562cac4
Kyle Lookreddy <35535638+complicationlamp@users.noreply.github.com>
2019-10-04 11:26:42 -0700

    Update ejs_ch_9.js

--- ejs_ch_9.js
+++ ejs_ch_9.js
@@ -40,3 +40,39 @@ function verify(regexp, yes, no) {
         console.log(`Unexpected match for '${str}'`);
     }
 }
+
+
+
+// Quoting style
+// Imagine you have written a story and used single quotation marks throughout to mark pieces of dialogue. Now you want to replace all the dialogue quotes with double quotes, while keeping the single quotes used in contractions like aren’t.
+
+// Think of a pattern that distinguishes these two kinds of quote usage and craft a call to the replace method that does the proper replacement.
+
+let text = "'I'm the cook,' he said, 'it's my job.'";
+
+// the ^ is the start or \W a non-word or a non-word or the end
+console.log(text.replace(/(^|\W)'|'(\W|$)/g,'$1"$2'));
+// → "I'm the cook," he said, "it's my job."
+
+
+
+
+// Numbers again
+// Write an expression that matches only JavaScript-style numbers. It must support an optional minus or plus sign in front of the number, the decimal dot, and exponent notation—5e-3 or 1E10—again with an optional sign in front of the exponent. Also note that it is not necessary for there to be digits in front of or after the dot, but the number cannot be a dot alone. That is, .5 and 5. are valid JavaScript numbers, but a lone dot isn’t.
+
+// Fill in this regular expression.
+let number = /^...$/;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
